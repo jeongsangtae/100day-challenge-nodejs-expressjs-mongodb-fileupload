c1514f05afbb9c93561ca14b4461ddbd0a21d3d0
jeongsangtae <wjdtkdxo55@naver.com>
2023-02-16 03:45:38 +0900

    NodeJS & MongoDB - multer

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 
-const userRoutes = require('./routes/users');
-const db = require('./data/database');
+const userRoutes = require("./routes/users");
+const db = require("./data/database");
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 app.use(userRoutes);
 
