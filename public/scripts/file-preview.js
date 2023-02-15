0c1fa44015bb2553b451ceea3ed99abdb615f8d9
jeongsangtae <wjdtkdxo55@naver.com>
2023-02-16 05:02:54 +0900

    JS & HTML, CSS - add image preview

---
+++ public/scripts/file-preview.js
@@ -0,0 +1,17 @@
+const fileElement = document.getElementById("image");
+const imagePreviewElement = document.getElementById("image-preview");
+
+function showPreview() {
+  const files = fileElement.files;
+  if (!files || files.length === 0) {
+    imagePreviewElement.style.display = "none";
+    return;
+  }
+
+  const pickedFile = files[0];
+
+  imagePreviewElement.src = URL.createObjectURL(pickedFile);
+  imagePreviewElement.style.display = "block";
+}
+
+fileElement.addEventListener("change", showPreview);
